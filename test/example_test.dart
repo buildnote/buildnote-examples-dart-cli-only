3bf3719e41fac12ea3593656b0b7de560e50b039
Albert Latacz <albert.latacz@gmail.com>
2025-03-01 16:43:43 +0000

    added failing test

--- test/example_test.dart
+++ test/example_test.dart
@@ -6,6 +6,11 @@ import 'package:test/test.dart';
 
 void main() {
   test('true is true', () {
+    print('hello from test');
     expect(true, isTrue);
+  });
+
+  test('true is not false', () {
+    expect(true, isFalse);
   });
 }
